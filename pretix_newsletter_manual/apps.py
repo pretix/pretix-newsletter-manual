87f717315b341bc0bdb579b6762ba18fd4526774
Raphael Michel <michel@rami.io>
2022-12-17 17:53:31 +0100

    Stop using deprecated Django APIs

---
+++ pretix_newsletter_manual/apps.py
@@ -0,0 +1,26 @@
+from django.utils.translation import gettext_lazy
+from . import __version__
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+
+class PluginApp(PluginConfig):
+    name = "pretix_newsletter_manual"
+    verbose_name = "Newsletter address collection (manual)"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Newsletter address collection (manual)")
+        author = "pretix team"
+        description = gettext_lazy("Manually collect newsletter registrations")
+        visible = True
+        version = __version__
+        category = "INTEGRATION"
+        compatibility = "pretix>=3.0.0"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
--- pretix_newsletter_manual/urls.py
+++ pretix_newsletter_manual/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import ManualSettings
 
 urlpatterns = [
-    url(
-        r"^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/newsletter_manual/settings$",
+    path(
+        "control/event/<str:organizer>/<str:event>/newsletter_manual/settings",
         ManualSettings.as_view(),
         name="settings",
     )
