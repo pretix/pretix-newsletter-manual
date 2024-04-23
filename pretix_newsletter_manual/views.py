3e9f59a3b684423d8774b4a773c2b6bbcff69473
Raphael Michel <michel@rami.io>
2024-04-23 14:42:56 +0200

    Use markdown text input widgets

--- pretix_newsletter_manual/views.py
+++ pretix_newsletter_manual/views.py
@@ -1,8 +1,10 @@
 import logging
+
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from i18nfield.forms import I18nFormField, I18nTextInput
-from pretix.base.forms import SettingsForm
+from i18nfield.forms import I18nFormField
+
+from pretix.base.forms import SettingsForm, I18nMarkdownTextInput
 from pretix.base.models import Event
 from pretix.control.views.event import EventSettingsFormView, EventSettingsViewMixin
 
@@ -11,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 class NewsletterSettingsForm(SettingsForm):
     newsletter_manual_text = I18nFormField(
-        label=_("Checkbox label"), required=True, widget=I18nTextInput
+        label=_("Checkbox label"), required=True, widget=I18nMarkdownTextInput
     )
 
 
